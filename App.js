35d399da4d67848893a35384a2d12883ec55e968
rafabelalcazar <rafaelb@unicauca.edu.co>
2020-01-10 19:31:31 -0500

    Uso de props

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
 import Card from "./src/components/Card";
-
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Card/>
-      
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Card
+          img="https://static3.depositphotos.com/1001890/175/i/950/depositphotos_1759729-stock-photo-sunflower-agriculture-background-beaut.jpg"
+          title="Propiedades del Girasol"
+          description="Las bondades del girasol son tatatata jas asldfj lasdkfjsal dfkjasdlf kasjdf laskdjf aslfd"
+        />
+        <Card
+          img="https://img.freepik.com/free-vector/abstract-particles-background-with-light-effect_1017-20135.jpg?size=626&ext=jpg"
+          title="Arte"
+          description="Las bondades del girasol son tatatata jas asldfj lasdkfjsal dfkjasdlf kasjdf laskdjf aslfd"
+        />
+      </View>
+    </ScrollView>
   );
 }
 
@@ -19,5 +28,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  },
+  }
 });
--- src/components/Card.js
+++ src/components/Card.js
@@ -5,23 +5,10 @@ export default class Card extends Component {
   render() {
     return (
       <View style={styles.cardContainer}>
-        <Image
-          style={styles.img}
-          source={{
-            uri:
-              "https://static3.depositphotos.com/1001890/175/i/950/depositphotos_1759729-stock-photo-sunflower-agriculture-background-beaut.jpg"
-          }}
-        />
+        <Image style={styles.img} source={{ uri: this.props.img }} />
         <View style={{ padding: 20 }}>
-          <Text style={styles.title}>Titulo</Text>
-          <Text style={styles.description}>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry standard dummy text ever
-            since the 1500s, when an unknown printer took a galley of type and
-            scrambled it to make a type specimen book. It has survived not only
-            five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged.
-          </Text>
+          <Text style={styles.title}>{this.props.title}</Text>
+          <Text style={styles.description}>{this.props.description}</Text>
         </View>
       </View>
     );
@@ -29,38 +16,31 @@ export default class Card extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      padding: 10,
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center"
+  cardContainer: {
+    margin: 10,
+    height: 400,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1
     },
-    cardContainer: {
-      height:400,
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 1
-      },
-      shadowOpacity: 0.2,
-      shadowRadius: 1.41,
-      elevation: 10,
-      backgroundColor: "white",
-      borderRadius: 10
-    },
-    img:{
-      flex:1,
-      borderTopLeftRadius: 10,
-      borderTopRightRadius: 10,
-    },
-    title:{
-      fontSize: 20,
-      fontWeight:'bold',
-      color: "#555"
-    },
-    description:{
-      color: '#555'
-    }
-  });
-  
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 10,
+    backgroundColor: "white",
+    borderRadius: 10
+  },
+  img: {
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#555"
+  },
+  description: {
+    color: "#555"
+  }
+});
